2c6328b7ae2d68239e14adf186942696d9827c4a
R4M1Z <p4tr0n0.dat@gmail.com>
2020-03-20 10:35:53 +0400

    Edit this

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,11 @@
 <?php
-$conn=mysqli_connect('localhost:3306','root','','data');
+# Edit these variables:
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="data";
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
   echo "ERROR: ".mysqli_connect_error($conn);
 }
