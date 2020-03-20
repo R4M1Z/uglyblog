9d3719a6db76c399d02c57d457559c2ebffd6d7b
R4M1Z <p4tr0n0.dat@gmail.com>
2020-03-20 10:42:31 +0400

    Edit this

--- config/db_connect.php
+++ config/db_connect.php
@@ -4,7 +4,6 @@ $servername="localhost";
 $username="root";
 $password="";
 $dbname="data";
-
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
   echo "ERROR: ".mysqli_connect_error($conn);
